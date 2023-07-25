8acc4e46a3bbf338a243a8f94574363c5c78e46f
roq-ai <>
2023-07-25 05:44:14 +0000

    Initial commit

---
+++ src/interfaces/expenditure/index.ts
@@ -0,0 +1,24 @@
+import { OrganizationInterface } from 'interfaces/organization';
+import { UserInterface } from 'interfaces/user';
+import { GetQueryInterface } from 'interfaces';
+
+export interface ExpenditureInterface {
+  id?: string;
+  amount: number;
+  description?: string;
+  organization_id?: string;
+  user_id?: string;
+  created_at?: any;
+  updated_at?: any;
+
+  organization?: OrganizationInterface;
+  user?: UserInterface;
+  _count?: {};
+}
+
+export interface ExpenditureGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  description?: string;
+  organization_id?: string;
+  user_id?: string;
+}
